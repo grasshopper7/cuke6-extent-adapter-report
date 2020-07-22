58eb912a329cf02d5e22af6267e79653e0cac2dd
grasshopper7 <grass.hopper.moc@gmail.com>
2020-07-22 15:51:34 +0530

    Updates for version 1.1.0

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-For more details refer to this [article](http://grasshopper.tech/2098/)
+For more details refer to this [article](https://grasshopper.tech/2098/)
--- cuke6-extent-adapter-report/src/test/java/stepdefs/RunCukeIT.java
+++ cuke6-extent-adapter-report/src/test/java/stepdefs/RunCukeIT.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(plugin = {
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }/* , tags="@SOut" */ )
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }/* , strict="true" */ )
 public class RunCukeIT extends AbstractTestNGCucumberTests {
 
 }
